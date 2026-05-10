0c12cb78c9ec15d9869d00c86b21ce70effd696a
Nova <nova.wittenhannah@gmail.com>
2026-05-11 10:45:08 +1200

    Add database scaffold

--- src/main.py
+++ src/main.py
@@ -3,4 +3,14 @@ import scipy as sp
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import sqlite3
+
+path = input('Enter the path of new data: ')
+
+df = pd.read_csv(path)
+conn = sqlite3.connect('database.db')
+
+df.to_sql('my_table', conn, if_exists='replace', index=False)
+
+conn.close()
 
